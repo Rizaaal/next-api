62f55e159a4c18fd8b1404937729c8a5f1dbe76b
Gabriele Di Grazia <gabrii539@gmail.com>
2024-03-02 10:44:51 +0100

    refactor: removed unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
+import { getData } from "./api/products";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { InferGetServerSidePropsType } from "next";
-import { link } from "fs";
-import { getData } from "./api/products";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -43,7 +41,7 @@ export default function Home({ products }: { products: Product[] }) {
             <li key={product.id}>
               <div className={styles.card}>
                 <h2 style={{ height: '180px' }}>{product.title}</h2>
-                <img src={product.thumbnail} alt="product image" />
+<img src={product.thumbnail} alt="product image" />
               </div>
             </li>
           ))}
